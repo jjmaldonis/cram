e37e14dbff9a4ed145c8a11f018e61d7ddbe6b8c
refreshx2 <jjmaldonis@gmail.com>
2013-08-12 17:28:56 -0500

    Now the file update works. Create a backup though just in case! Do a comparison the first few times.

--- cram.cpp
+++ cram.cpp
@@ -16,35 +16,66 @@ unsigned long Mex(bool used_values[], unsigned long max);
 //void MakeAllFalse(bool used_values[]);
 int main()
 {
+    printf("HERE1");
 	char writefilename[64];
-	snprintf(writefilename, 64, "gv_%d", MAXVALUE);
+	snprintf(writefilename, 64, "qsub_%lu", MAXVALUE);
 	FILE* writefile;
-    //ofstream writefile(writefilename);
 	// Create the 4 arrays of size MAXVALUE and a boolean "used" array for mexing
-	unsigned long sprague_n[MAXVALUE];
-	unsigned long sprague_s[MAXVALUE];
-	unsigned long sprague_l[MAXVALUE];
-	unsigned long sprague_u[MAXVALUE];
+	unsigned long sprague_n[MAXVALUE] = {0};
+	unsigned long sprague_s[MAXVALUE] = {0};
+	unsigned long sprague_l[MAXVALUE] = {0};
+	unsigned long sprague_u[MAXVALUE] = {0};
 	// Initallize all values to FALSE in used_values array.
 	bool used_values[MAXVALUE] = { 0 };
-	char buffer[64];
+	char line[64];
+    printf("HERE2");
+
+    snprintf(line, 64, "\0");
 
     // For now, just erase the previous file until I get it to read in and initialize the starting point.
-    writefile = fopen( writefilename, "w" );
-    fclose(writefile);
+    //writefile = fopen( writefilename, "w" );
+    //fclose(writefile);
+
+	unsigned long i = 0;
+	unsigned long j = 0;
 
 	// Open the file to read in the current contents if any exist.
-	writefile = fopen( writefilename, "r" );
-	unsigned long i = 0;
-	while( fgets( buffer, 64, writefile ) != NULL )
-	{
-		sprague_n[i] = atoi( strtok (buffer,",") );
-		sprague_s[i] = atoi( strtok (buffer,",") );
-		sprague_l[i] = atoi( strtok (buffer,",") );
-		sprague_u[i] = atoi( strtok (buffer,",") );
-		i++;
-	}
-	fclose(writefile);
+    printf("HERE3");
+	if(writefile = fopen( writefilename, "r" ))
+    {
+        printf("File open successfully.\n");
+        while( fgets( line, 64, writefile ) != NULL )
+        {
+            char * pch;
+        	pch = strtok (line,",");
+        	if(pch != NULL) i = atoi( pch );
+            pch = strtok (NULL, ",");
+        	if(pch != NULL) sprague_n[i] = atoi( pch );
+            pch = strtok (NULL, ",");
+        	if(pch != NULL) sprague_s[i] = atoi( pch );
+            pch = strtok (NULL, ",");
+        	if(pch != NULL) sprague_l[i] = atoi( pch );
+            pch = strtok (NULL, ",");
+        	if(pch != NULL) sprague_u[i] = atoi( pch );
+            //printf("%lu, %lu, %lu, %lu, %lu\n", i, sprague_n[i], sprague_s[i], sprague_l[i], sprague_u[i]);
+        }
+        fclose(writefile);
+        // Rewrite file because we may have found a line at the end that was screwed up.
+        writefile = fopen(writefilename, "w" );
+        for(j = 0; j < i; j++)
+		    fprintf(writefile, "%lu,%lu,%lu,%lu,%lu\n", j, sprague_n[j], sprague_s[j], sprague_l[j], sprague_u[j] );
+        fclose(writefile);
+        printf("Re-write file complete.\n")
+    }
+    else
+    {
+        printf("There was a problem opening the file.\n");
+        writefile = fopen(writefilename, "w" );
+        fclose(writefile);
+    }
+    printf("HERE4");
+
+    // Ending this loop will leave us at the last i value read in. This data may not be correct because the data may not have finished writing, so we will start at this i value and overwrite it.
 
 	// Open the file for appending.
 	writefile = fopen( writefilename, "a" );
@@ -54,7 +85,7 @@ int main()
 	{
         printf("File did not already exist, creating a new one.\n");
 		// Create initial conditions
-		for(unsigned long i = 0; i < N; i++)
+		for(i = 0; i < N; i++)
 		{
 			sprague_n[i] = 0;
 			sprague_s[i] = 0;
@@ -65,24 +96,19 @@ int main()
 		sprague_s[N] = 1;
 		sprague_l[N] = 1;
 		sprague_u[N] = 0;
+        i++;
 
-		// Open the file for writing and write initial conditions to file.
-		//writefile = fopen( writefilename, "r" );
-
-		snprintf( buffer, 64, "0,%lu,%lu,%lu,%lu\n", sprague_n[0], sprague_s[0], sprague_l[0], sprague_u[0] );
-        cout << buffer << endl;
-		fprintf( writefile, buffer );
+		fprintf( writefile, "0,%lu,%lu,%lu,%lu\n", sprague_n[0], sprague_s[0], sprague_l[0], sprague_u[0] );
 		fprintf( writefile, "1,%lu,%lu,%lu,%lu\n", sprague_n[1], sprague_s[1], sprague_l[1], sprague_u[1] );
 		fprintf( writefile, "2,%lu,%lu,%lu,%lu\n", sprague_n[2], sprague_s[2], sprague_l[2], sprague_u[2] );
-		//writefile << 0 << "," << sprague_n[0] << "," << sprague_s[0] << "," << sprague_l[0] << "," << sprague_u[0] << endl;
-		//writefile << 1 << "," << sprague_n[1] << "," << sprague_s[1] << "," << sprague_l[1] << "," << sprague_u[1] << endl;
-		//writefile << 2 << "," << sprague_n[2] << "," << sprague_s[2] << "," << sprague_l[2] << "," << sprague_u[2] << endl;
 	}
     else
-        printf("File already existed.\n");
+        printf("File already existed. i = %lu\n",i);
 
 	// Calculate and write new values to file.
-	for(unsigned long i = N+1; i <= MAXVALUE; i++)
+    // i is preset in the read-in.
+    printf("i=%lu\n",i);
+	for(i; i <= MAXVALUE; i++)
 	{
 		//sprague_n
         #pragma omp for
@@ -120,10 +146,6 @@ int main()
 
 		// Send output to file for in the form N,n,s,l,u
 		fprintf(writefile, "%lu,%lu,%lu,%lu,%lu\n", i, sprague_n[i], sprague_s[i], sprague_l[i], sprague_u[i] );
-		// printf("%l,%l,%l,%l,%l\n", i, sprague_n[i], sprague_s[i], sprague_l[i], sprague_u[i] );
-        //writefile << i << "," << sprague_n[i] << sprague_s[i] << sprague_l[i] << sprague_u[i] << endl;
-		//writefile << i << "," << sprague_n[i] << "," << sprague_s[i] << "," << sprague_l[i] << "," << sprague_u[i] << endl;
-		//flush file???
 	}
 	fclose(writefile);
 	return 0;
